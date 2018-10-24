798f8d3201db5a2b8632ff769e9e7fad41d7e270
tt405907 <theo.tracol@etu.univ-cotedazur.fr>
2018-10-24 23:50:00 +0200

    ajout de la classe De

---
+++ src/main/java/de/De.java
@@ -0,0 +1,51 @@
+package de;
+
+import java.util.Arrays;
+import java.util.Random;
+import joueur.Joueur;
+/**
+ * Classe permettant de simuler un De pour un joueur , un De a 6 faces
+ */
+public class De
+{
+    //On stock les faces de notre De
+    private Face[] allFaces;
+    // Permet de generer un random pour notre lancer
+    private Random rand;
+
+    // Nos Des initaux
+    public static final Face[] de1 = {new FaceOr(1),new FaceOr(1),new FaceOr(1),new FaceOr(1),new FaceOr(1),new FaceSoleil(1)};
+    public static final Face[] de2 = {new FaceOr(1),new FaceOr(1),new FaceOr(1),new FaceOr(1),new FaceLune(1),new FaceVictoire(2)};
+
+    /**
+     * Notre constructeur pour initialiser nos 2 Des
+     * @param allFaces Notre De initial , de1 pour notre premier De  et de2 pour notre deuxieme De
+     */
+    public De (Face[] allFaces)
+    {
+        // On init nos Des
+        this.allFaces = Arrays.copyOf(allFaces,6);
+        rand = new Random();
+    }
+
+    /**
+     * Permettant la prise d'une face d'un de a l'aide d'un int (indice)
+     * @param indice l'indice de la face du De que l'on veut
+     * @return Retourne donne la face a l'indice mentionne
+     * */
+    public Face getFace(int indice)
+    {
+        return this.allFaces[indice];
+    }
+
+    /**
+     * Nous appliquons une Face de manière aléatoire sur les deux De
+     * @param j Le propietaire du De
+     */
+    public void appliquerDe(Joueur j)
+    {
+        int temp = rand.nextInt(6);
+        this.getFace(temp).appliquer(j);
+    }
+
+}
--- src/main/java/joueur/Joueur.java
+++ src/main/java/joueur/Joueur.java
@@ -1,5 +1,7 @@
 package joueur;
 
+import de.De;
+import de.Face;
 public class Joueur {
 	private int or, orMax;
 	private int soleil, soleilMax;
@@ -8,6 +10,7 @@ public class Joueur {
 
 	// Pour l'affichage
 	private String nom;
+	private De de1, de2;
 
 	public Joueur(String nom) {
 		super();
@@ -15,32 +18,48 @@ public class Joueur {
 		orMax = 12;
 		soleilMax = 6;
 		luneMax = 6;
+
+		// Des initaux
+		de1 = new De(De.de1);
+		de2 = new De(De.de2);
 	}
 
 	public int getOr() {
 		return or;
 	}
+
 	public int getSoleil() {
 		return soleil;
 	}
+
 	public int getLune() {
 		return lune;
 	}
+
 	public int getVictoire() {
 		return victoire;
 	}
-	
+
 	public void addOr(int or) {
 		this.or = Math.min(orMax, Math.max(0, this.or + or));
 	}
+
 	public void addSoleil(int soleil) {
 		this.soleil = Math.min(soleilMax, Math.max(0, this.soleil + soleil));
 	}
+
 	public void addLune(int lune) {
 		this.lune = Math.min(luneMax, Math.max(0, this.lune + lune));
 	}
+
 	public void addVictoire(int victoire) {
 		this.victoire += victoire;
+	}
+
+
+	public void appliquerDe() {
+		de1.appliquerDe(this);
+		de2.appliquerDe(this);
 	}
 
 }
