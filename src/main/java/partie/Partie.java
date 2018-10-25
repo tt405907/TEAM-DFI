28cffb5468bec64660a79be4ded78c33329bedb7
arbona arnaud <aa303554@baobab.unice.fr>
2018-10-25 14:09:24 +0200

    Partie

---
+++ src/main/java/partie/Partie.java
@@ -0,0 +1,63 @@
+package partie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import joueur.Joueur;
+
+public class Partie {
+	private Joueur[] joueurs;
+	private int nbTour;
+
+	public Partie(Joueur... joueurs) {
+		this.joueurs = joueurs;
+		if ( joueurs.length == 3) {
+			nbTour = 10;
+		}
+		else {
+			nbTour = 9;
+		}
+	}
+	
+	/**effectuer un tour pour chaque joueurs
+	 * 
+	 */
+	public void faireTours() {
+		for (Joueur act : joueurs) {
+			for (Joueur j : joueurs) {
+				j.appliquerDe();
+				if (joueurs.length == 2) {
+					j.appliquerDe();
+				}
+			}
+			//TODO action joueurs
+		}
+		nbTour--;
+	}
+	
+	/**
+	 * commence la partie et renvoie les gagnants
+	 */
+	public List<Joueur> game() {
+		for (int i = 0; i < joueurs.length; i++) {
+			joueurs[i].addOr(4-i);
+		}
+		while (nbTour > 0) {
+			faireTours();
+		}
+		List<Joueur> gagnant = new ArrayList<>();
+		int max = 0;
+		for (Joueur j : joueurs) {
+			if (j.getVictoire() > max) {
+				gagnant.clear();
+				max = j.getVictoire();
+				gagnant.add(j);
+			}
+			else if (j.getVictoire() == max) {
+				gagnant.add(j);
+			}
+		}
+		return gagnant;
+	}
+	
+}
